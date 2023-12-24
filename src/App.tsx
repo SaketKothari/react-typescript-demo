2e9f26c5c7340bb86de3e047ce3f6a96b54bd97e
Saket Kothari <saketkothari722@gmail.com>
2023-12-24 22:07:18 +0530

    defining component prop types

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import CourseGoal from './components/CourseGoal';
+
+const App = () => {
+  return (
+    <main>
+      <CourseGoal
+        title="Learn React + TS"
+        description="Learn from the ground up"
+      />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,22 @@
+const CourseGoal = (
+  // Destructuring
+  {
+    title,
+    description,
+  }: {
+    title: string;
+    description: string;
+  }
+) => {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+};
+
+export default CourseGoal;
