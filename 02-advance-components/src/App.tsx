1cd92b029b823cb1a26614a97967121bc99c1df8
Saket Kothari <saketkothari722@gmail.com>
2023-12-29 11:01:47 +0530

    building a wrapper component that renders different elements

--- 02-advance-components/src/App.tsx
+++ 02-advance-components/src/App.tsx
@@ -1,3 +1,4 @@
+import Button from './components/Button';
 import Input from './components/Input';
 
 function App() {
@@ -5,6 +6,15 @@ function App() {
     <main>
       <Input id="name" label="Your name" type="text" />
       <Input id="age" label="Your age" type="number" />
+
+      <p>
+        <Button element="button">Button</Button>
+      </p>
+      <p>
+        <Button element="anchor" href="https://google.com" target="_blank">
+          A Link
+        </Button>
+      </p>
     </main>
   );
 }
