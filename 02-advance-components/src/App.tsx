4ca3bb4ec9d7ae47f6aceec19e4faae63dd6bce5
Saket Kothari <saketkothari722@gmail.com>
2024-01-02 21:45:18 +0530

    building another wrapper component custom form

--- 02-advance-components/src/App.tsx
+++ 02-advance-components/src/App.tsx
@@ -1,11 +1,17 @@
-import { useRef } from 'react';
+import Button from './components/Button';
 import Input from './components/Input';
+import Form from './components/Form';
 
 function App() {
-  const inputRef = useRef<HTMLInputElement>(null);
   return (
     <main>
-      <Input id="test" label="Test" ref={inputRef} />
+      <Form>
+        <Input id="name" type="text" label="Name" />
+        <Input id="age" type="number" label="Age" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
